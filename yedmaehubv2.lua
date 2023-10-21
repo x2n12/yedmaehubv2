b7909f6a237f3750475cc26b9290bbbb2e17ce5a
x2n12 <147017566+x2n12@users.noreply.github.com>
2023-10-21 20:55:10 +0700

    ESPARUN.lua

--- yedmaehubv2.lua
+++ yedmaehubv2.lua
@@ -1,10 +1,30 @@
-getgenv().config = getgenv().config or {
-	hit_time = 0.5, -- // recommended 0.25 to 0.75 \\ --
-	
-	mode = 'Always', -- // Hold , Toggle , Always \\ --
-	deflect_type = 'Remote', -- // Key Press , Remote \\ --
-	notifications = true,
-	keybind = Enum.KeyCode.V
-}
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/MC%3ABlade%20Ball%20Parry%20V4.0.0",true))()
+--ESPบ้านตาดํา by YOMA HUB
+while wait() do
+     pcall(function()
+       for i,v in pairs(game.Players:GetChildren()) do
+            if not v.Character.Head:FindFirstChild("ESP") then
+                local BillboardGui = Instance.new("BillboardGui")
+                local TextLabel = Instance.new("TextLabel")
+                BillboardGui.Parent = v.Character.Head
+                BillboardGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+                BillboardGui.Active = true
+                BillboardGui.Name = "ESP"
+                BillboardGui.AlwaysOnTop = true
+                BillboardGui.LightInfluence = 1.000
+                BillboardGui.Size = UDim2.new(0, 200, 0, 50)
+                BillboardGui.StudsOffset = Vector3.new(0, 2.5, 0)
+                TextLabel.Parent = BillboardGui
+                TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+                TextLabel.BackgroundTransparency = 1.000
+                TextLabel.Size = UDim2.new(0, 200, 0, 50)
+                TextLabel.Font = Enum.Font.GothamBold
+                TextLabel.Text = v.Name
+                TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+                TextLabel.TextScaled = true
+                TextLabel.TextSize = 14.000
+                TextLabel.TextStrokeTransparency = 0.000
+                TextLabel.TextWrapped = true
+            end
+        end
+    end) 
+end
