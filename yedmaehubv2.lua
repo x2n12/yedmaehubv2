2e1404a871ff63e205780cfecd41e9b7746fb3dd
x2n12 <147017566+x2n12@users.noreply.github.com>
2023-12-17 21:08:13 +0700

    Update yedmaehubv2.lua

--- yedmaehubv2.lua
+++ yedmaehubv2.lua
@@ -1,30 +1,74 @@
---ESPบ้านตาดํา by YOMA HUB
-while wait() do
-     pcall(function()
-       for i,v in pairs(game.Players:GetChildren()) do
-            if not v.Character.Head:FindFirstChild("ESP") then
-                local BillboardGui = Instance.new("BillboardGui")
-                local TextLabel = Instance.new("TextLabel")
-                BillboardGui.Parent = v.Character.Head
-                BillboardGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-                BillboardGui.Active = true
-                BillboardGui.Name = "ESP"
-                BillboardGui.AlwaysOnTop = true
-                BillboardGui.LightInfluence = 1.000
-                BillboardGui.Size = UDim2.new(0, 200, 0, 50)
-                BillboardGui.StudsOffset = Vector3.new(0, 2.5, 0)
-                TextLabel.Parent = BillboardGui
-                TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-                TextLabel.BackgroundTransparency = 1.000
-                TextLabel.Size = UDim2.new(0, 200, 0, 50)
-                TextLabel.Font = Enum.Font.GothamBold
-                TextLabel.Text = v.Name
-                TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-                TextLabel.TextScaled = true
-                TextLabel.TextSize = 14.000
-                TextLabel.TextStrokeTransparency = 0.000
-                TextLabel.TextWrapped = true
+local RunService, UserInputService, TweenService = game.RunService, game.UserInputService, game.TweenService
+local Player, ReplicatedStorage, Debris = game.Players.LocalPlayer, game.ReplicatedStorage, game.Debris
+
+local Remotes = {
+    ThunderDash = ReplicatedStorage.Remotes.ThunderDash, -- // ThunderDash Endpoint, Table where parts are put (just put random cframes) FIRETYPE = :FireServer()
+    Emote = ReplicatedStorage.Remotes.CustomEmote, -- // Boolean / true or false, Emote / 'Emperyean or sumn' and 'Wavelight' FIRETYPE = :FireServer()
+    Parry = ReplicatedStorage.Remotes.ParryButtonPress -- // nil / no tuple FIRETYPE = :Fire()
+}
+
+local bug_ball_method_____________________________________init = false
+local ParryCD = false
+local Parry = false
+local Visual = false
+
+
+local HitboxPart = Instance.new('Part', workspace)
+HitboxPart.Color = Color3.fromHex('#FFA500')
+HitboxPart.Anchored = true
+HitboxPart.Material = Enum.Material.SmoothPlastic
+HitboxPart.Shape = Enum.PartType.Ball
+HitboxPart.CanCollide = false
+HitboxPart.CastShadow = false
+HitboxPart.Transparency = 0.75
+
+local function Parry(OBJ)
+    local function getplayerids()
+        local ids = {}
+        for i, player in pairs(game.Players:GetPlayers()) do
+            ids[player.UserId] = player.Character.Head
+        end
+        return ids
+    end
+    if not bug_ball_method_____________________________________init then
+        Remotes.Parry:Fire()
+    else
+        game.ReplicatedStorage.Remotes.ParryAttempt:FireServer(0.5, Player.Character.HumanoidRootPart.CFrame, getplayerids(), {100, 100})
+    end
+    ParryCD = true
+    OBJ:SetAttribute('target', '')
+    task.delay(.1, function()
+        ParryCD = false
+    end)
+end
+RunService.Stepped:Connect(function(Time, DeltaTime)
+    for i, ball in pairs(workspace.Balls:GetChildren()) do
+        if ball:GetAttribute('realBall') then
+            local distance = (Player.Character.HumanoidRootPart.Position - ball.Position).Magnitude
+            local ballVelocity = ball.Velocity
+            local ballMagnitude = ballVelocity.Magnitude / 3
+            local ballVolume = ball.Velocity.X + ball.Velocity.Y + ball.Velocity.Z
+            if Visual then
+                HitboxPart.Position = Player.Character.HumanoidRootPart.Position
+                if ballVolume >= 1 then
+                    HitboxPart.Size = Vector3.new(ballVolume, ballVolume, ballVolume)
+                elseif ballVolume <= 5 then
+                    HitboxPart.Size = Vector3.new(20, 20, 20)
+                else
+                    HitboxPart.Size = -Vector3.new(ballVolume, ballVolume, ballVolume)
+                end
+            else
+                HitboxPart.Position = Vector3.new(0, 100000, 0)
+            end
+            if ball:GetAttribute('target') == (Player.Name or Player.DisplayName) and not ParryCD then
+                if Parry then
+                    if distance <= ballMagnitude or distance <= 20 then
+                        Parry(ball)
+                    else
+                        assert(true, 'if u skid u bad :grin:')
+                    end
+                end
             end
         end
-    end) 
-end
+    end
+end)
